7e7a3e5140ae7775e4b5e1fb2062c07ee6cbbc0b
Kiều Trọng Thiện <kieutrongthien@gmail.com>
2022-11-28 23:39:19 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b66fcefff0b647a78803b640cf328546.js"
+  "https://kieutrongthien.github.io/kieutrongthien/precache-manifest.585d6bd4ec768335ce40d7b6e8f22a93.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://kieutrongthien.github.io/kieutrongthien/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
