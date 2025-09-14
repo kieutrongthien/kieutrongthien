4b9bb91e212448dc2374a800cf4d99f91ce8b40e
Bieber Kieu <kieutrongthien@gmail.com>
2025-09-15 01:20:15 +0700

    Updates

---
+++ precache-manifest.978305220960c45952abf12a76008525.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6cf9852717ff89737c044662b30ab85",
+    "url": "/kieutrongthien/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/kieutrongthien/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "89d4adbac94ff4f57721",
+    "url": "/kieutrongthien/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/kieutrongthien/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/kieutrongthien/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89d4adbac94ff4f57721",
+    "url": "/kieutrongthien/static/js/main.6e8cb2f2.chunk.js"
+  },
+  {
+    "revision": "c0516ad3bdbd1cd22b32",
+    "url": "/kieutrongthien/static/js/runtime-main.39bfa5a1.js"
+  },
+  {
+    "revision": "20e87a8fdcc9e4843845f44946a9c6b2",
+    "url": "/kieutrongthien/static/media/resume.20e87a8f.pdf"
+  }
+]);
